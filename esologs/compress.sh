8085d647d3ee9a2173947b80490ed02604d473bc
Heikki Kallasjoki <fis@zem.fi>
2017-12-27 01:30:35 +0000

    Add batch job to brotli-compress IRC logs.

---
+++ esologs/compress.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -e
+
+# batch job to Brotli-compress old log files
+
+cutoff="$(expr $(date +%s) - 172800)"
+
+find /home/esowiki/logs -name '*.pb' | while read logfile; do
+    if [[ "$logfile" =~ ^/home/esowiki/logs/([0-9]+)/([0-9]+)/([0-9]+)\.pb$ ]]; then
+        logtime="$(TZ=UTC0 date --date="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}-${BASH_REMATCH[3]}" +%s)"
+        if (( logtime < cutoff )); then
+            brotli --quality 11 < "$logfile" > "${logfile}.br"
+            if diff -q "$logfile" <(brotli --decompress < "${logfile}.br"); then
+                rm "$logfile"
+            else
+                echo "compressed logfile differs: $logfile" >&2
+                exit 1
+            fi
+        fi
+    else
+        echo "invalid logfile path: $logfile" >&2
+        exit 1
+    fi
+done
