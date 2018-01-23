1632b7cd2151615291bd96e7bfe5e4e4d198b26a
Heikki Kallasjoki <fis@zem.fi>
2018-01-23 03:30:32 +0000

    Remove dynamic module loading for unnecessary complexity.

--- esobot/esobot.cc
+++ esobot/esobot.cc
@@ -1,7 +1,26 @@
 #include "base/log.h"
 #include "esobot/config.pb.h"
+#include "esobot/logger.h"
+#include "esobot/rcfeed.h"
 #include "irc/bot/bot.h"
 #include "proto/util.h"
+
+namespace esobot {
+
+class EsoBot : public irc::bot::ConfiguredBot<Config> {
+ public:
+  explicit EsoBot(const char* config_file);
+
+ private:
+  const char* config_file_;
+};
+
+EsoBot::EsoBot(const char* config_file) : ConfiguredBot(config_file) {
+  RegisterPlugin<LoggerConfig, Logger>();
+  RegisterPlugin<RcFeedConfig, RcFeed>();
+}
+
+} // namespace esobot
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -9,13 +28,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char *config_file = argv[1];
-  irc::bot::Bot bot(
-      [&config_file]() {
-        auto config = std::make_unique<esobot::Config>();
-        proto::ReadText(config_file, config.get());
-        return config;
-      });
+  esobot::EsoBot bot(argv[1]);
   bot.Run();
   return 0;
 }
