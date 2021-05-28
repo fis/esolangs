989ef4a7c150fa4540a4cb4e5fad9d5a900137b4
Heikki Kallasjoki <fis+github@zem.fi>
2021-05-28 02:19:20 +0100

    Include the remote control interface in esobot.

--- esobot/esobot.cc
+++ esobot/esobot.cc
@@ -4,11 +4,13 @@
 #include "esobot/logger.h"
 #include "esobot/rcfeed.h"
 #include "irc/bot/bot.h"
+#include "irc/bot/remote.h"
 #include "proto/util.h"
 #include <memory>
 
 int main(int argc, char *argv[]) {
   irc::bot::Bot bot;
+  irc::bot::RegisterRemoteModule(&bot);
   bot.RegisterModule<esobot::BridgeConfig, esobot::Bridge>();
   bot.RegisterModule<esobot::LoggerConfig, esobot::Logger>();
   bot.RegisterModule<esobot::RcFeedConfig, esobot::RcFeed>();
